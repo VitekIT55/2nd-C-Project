121c267c7cff6463d19f3df598e311cde7d2a2d7
VitekIT55 <trakmanic80@gmail.com>
2023-12-23 20:51:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std; 
+int main()
+{
+//    setlocale(LC_ALL, "Russian");
+    system("chcp 1251>nul");
+    string text, a, b;
+    cout << "Введите текст: "; cin >> text;
+    int length = text.length();
+    a = text[0];
+    b = text[length - 1];
+    cout << "//Результат//\nВведённая строка: " << text << "\nДлинна строки: " << length << endl;
+    cout << "Первый символ: " << a << endl;
+    cout << "Последний символ: " << b << endl;
+}
+
